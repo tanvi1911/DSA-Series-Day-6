b22a19eb536d53d8858e80a55bd3feda8a21ddf6
Tanvi Bhabal <81086155+tanvi1911@users.noreply.github.com>
2023-03-20 17:34:33 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+/*
+REVERSE A NUMBER
+Steps 1: input the number from user which is t be reversed
+Step 2: initialize a variable named 'ans'
+Step 3: Start a while loop till the input number becomes 0
+Step 4: Take out the last digit from the input number
+Step 5: apply the foll formula: ans = (ans *10) + digit
+Step 6: divide the input no. so that we will get updated version of the number
+Step 7: Print the output
+
+*/
+
+
+int main() {
+//   int n;
+//   cin >> n;
+//   int ans = 0;
+
+//   while(n!=0) {
+//     int digit = n % 10;
+//     ans = (10*ans) + digit;
+//     n = n / 10;
+//   }
+//   cout << ans;
+
+int n;
+  cout << "Enter a number";
+cin >> n;
+int m = n;
+int mask = 0;
+
+while (m!=0) {
+  m 
+}
+
+
+
+}
